a0856cb9f5a1d7ae806b8fc6258d6dccd5569677
Andreas Marsh <marsh.andreas.s@gmail.com>
2021-09-28 13:42:29 -0400

    Initial Commit

---
+++ Find My Fish Beta/Find_My_Fish_BetaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Find_My_Fish_BetaApp.swift
+//  Find My Fish Beta
+//
+//  Created by NMI Capstone on 9/28/21.
+//
+
+import SwiftUI
+
+@main
+struct Find_My_Fish_BetaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
